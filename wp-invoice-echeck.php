7021f2652ce080f4378460c64f65397aa988c900
Anton Korotkov <antonkorotkoff@gmail.com>
2017-08-22 12:17:14 +0300

    version bump

--- wp-invoice-echeck.php
+++ wp-invoice-echeck.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.usabilitydynamics.com
  * Description: Authorize.net eCheck
  * Author: Usability Dynamics, Inc.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Text Domain: wpi-echeck
  * Author URI: http://usabilitydynamics.com
  *
